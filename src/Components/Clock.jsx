0b0cedf439ec71442595057dff905db8bc73bf5f
Virenk21 <viren.d222111@gmail.com>
2022-03-21 23:59:51 +0530

    digit updated nd css

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -30,13 +30,26 @@ class Clock extends Component {
         </div>
 
         <div className="min_hand"
-          style={{transform : `rotateZ(${this.state.time.getHours() *6}deg)`}}>
+          style={{transform : `rotateZ(${this.state.time.getMinutes() *6}deg)`}}>
         </div>
 
          <div className="sec_hand"
-          style={{transform : `rotateZ(${this.state.time.getHours() *6}deg)`}}>
+          style={{transform : `rotateZ(${this.state.time.getSeconds() *6}deg)`}}>
         </div>
              
+             <span className="twelve">12</span>
+             <span className="one">1</span>
+              <span className="two">2</span>
+             <span className="three">3</span>
+              <span className="four">4</span>
+              <span className="five">5</span>
+             <span className="six">6</span>
+            <span className="seven">7</span>
+        <span className="eight">8</span>
+        <span className="nine">9</span>
+        <span className="ten">10</span>
+        <span className="eleven">11</span>
+        
     </div>
     </>
     );
